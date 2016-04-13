faada4e4c4d0eb986a703e191dce07949a42e7b1
Kyung yul Kevin Lim <thekoalaz@gmail.com>
2016-04-13 14:38:28 -0700

    Added line-by-line comment explanations.

--- app.js
+++ app.js
@@ -1,22 +1,31 @@
-﻿var path = require('path');
+﻿/* Standard js modules */
+var path = require('path');
 
+/* express modules */
 var express = require('express');
 var exphbs = require('express-handlebars');
 
-var routes = require('./routes/index');
-var index = require('./routes/index');
-
+/* Create our express app */
 var app = express();
 
-// view engine setup
+/* View engine setup
+ * We don't setup a defaultLayout, and change the extension name from the
+ * default .handlebars to the shorter .hbs
+ */
 app.set('views', path.join(__dirname, 'views'));
 app.engine('.hbs', exphbs({ defaultLayout: false, extname: '.hbs' }));
 app.set('view engine', '.hbs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
+/* Static path setup
+ * Anything in the public/ folder is now served as static content.
+ */
 app.use(express.static(path.join(__dirname, 'public')));
 
+/* Setup your routes here.
+ * The basic form is to require the js file, and call the view function from
+*/
+var index = require('./routes/index');
 app.use('/', index.view);
 
+/* Export our app so that bin/www can run it. */
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,4 @@
 ﻿/* GET home page. */
 exports.view = function (req, res) {
-    res.render('index', { title: 'Express' });
+    res.render('index');
 };
